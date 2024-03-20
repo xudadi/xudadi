611d5654e872039263154098bda722b3e9d16e5c
Xudadi <xudadi811@gmail.com>
2024-03-20 18:41:13 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def get_link_info(feed_url, num):
         title = entrie["title"]
         link = entrie["link"]
         # result = result + "\n" + "[" + title + "](" + link + ")" + "\n"
-        result = result + "\n" + "[" + title + "](" + link + "+ 'target="_blank")" + "\n"
+        result = result + "\n" + "[" + title + "](" + link + " 'target="_blank")" + "\n"
     
     return result
     
