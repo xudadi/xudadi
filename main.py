14d1927a9ecb5a3a7e70963c71d996f773e754be
Xudadi <xudadi811@gmail.com>
2024-12-12 09:27:53 +0800

    Update main.py

--- main.py
+++ main.py
@@ -31,9 +31,9 @@ def get_link_info(feed_url, num):
 
 def main():
 
-    xudadi_info =  get_link_info("https://lab.xudadi.cn/feeds/cqgkld.xml", 3)
+    xudadi_info =  get_link_info("https://lab.xudadi.cn/feeds/cqgkld.xml", 5)
     print(xudadi_info)
-    garss_info =  get_link_info("https://lab.xudadi.cn/feeds/163news.xml", 3)
+    garss_info =  get_link_info("https://lab.xudadi.cn/feeds/163news.xml", 5)
     print(garss_info)
 
     insert_info = xudadi_info + garss_info
